d71ce3c55fb554a7a08018cbbc92d7b1ede710da
johnmark14 <johmarkgabrentina@gmail.com>
2021-08-10 13:22:23 +0800

    bounded data to elements

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ const app = Vue.createApp({
             rotateZ: 0,
         }
     },
+    computed: {
+        box() {
+            return {
+                transform: `perspective(${this.perspective}px) 
+                rotateX(${this.rotateX}deg) 
+                rotateY(${this.rotateY}deg)
+                rotateZ(${this.rotateZ}deg)`
+            }
+        }
+    },
+    methods: {
+        reset() {
+            this.perspective = 100
+            this.rotateX = 0
+            this.rotateY = 0
+            this.rotateZ = 0
+        }
+    }
 }).mount('#app')
